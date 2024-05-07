37b34f6a65419c9b084488c3ea994f9e3b38d2f1
Panuwat <netpopo1150@outlook.com>
2024-05-07 11:55:59 +0700

    create togglr cart

--- src/Components/NavbarComp/Navbar/NavbarRight/NavbarRight.jsx
+++ src/Components/NavbarComp/Navbar/NavbarRight/NavbarRight.jsx
@@ -6,8 +6,8 @@ import UserNav from "./UserNav";
 import { isLogin } from "../../../../Store/UserSlice";
 
 function NavbarRight() {
-  const [isPopup, setIsPopUp] = useState(false);
-  // const [isLogin, setIsLogin] = useState(login);
+  const [isPopup, setIsPopup] = useState(false);
+  const [popupCart, setPopupCart] = useState(false);
   const login = useSelector((state) => state.userIsLogin.login);
   const dispatch = useDispatch();
 
@@ -24,17 +24,19 @@ function NavbarRight() {
     <div className="flex gap-8 items-center">
       {/* categories */}
       <div>
-        <Link to={"/categories"} className="text-xl font-semibold">
+        <Link to={"/categories"} className="text-xl font-bold">
           Products
         </Link>
       </div>
       {/* Account Login */}
       {login ? (
+        // Login is true
         <UserNav />
       ) : (
+        // is not Login
         <div
           className="flex gap-2 items-center cursor-pointer"
-          onClick={() => setIsPopUp(true)}
+          onClick={() => setIsPopup(true)}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -53,22 +55,48 @@ function NavbarRight() {
           <p className="text-2xl text-gray-500">Login</p>
         </div>
       )}
-
-      {isPopup && <LoginPopUp onClose={() => setIsPopUp(false)} />}
+      {isPopup && <LoginPopUp onClose={() => setIsPopup(false)} />}
 
       {/* Shopping Basket */}
-      <button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="36"
-          height="36"
-          fill="currentColor"
-          className="bi bi-cart3"
-          viewBox="0 0 16 16"
-        >
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-        </svg>
-      </button>
+      {popupCart ? (
+        <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-end">
+          <div className="bg-white w-full max-w-sm p-5">
+            <div className="flex flex-row justify-between items-center">
+              <h3 className="text-2xl font-bold ">Your Cart</h3>
+              <button onClick={() => setPopupCart(false)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="42"
+                  fill="currentColor"
+                  className="bi bi-x"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                </svg>
+              </button>
+            </div>
+            {/* for add Product in Cart */}
+            <div className="flex flex-col justify-center items-center h-full">
+              <p className="text-2xl">Your cart is empty</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="relative" onClick={() => setPopupCart(true)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="30"
+            fill="grey"
+            className="bi bi-cart3"
+            viewBox="0 0 16 16"
+          >
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+          </svg>
+          <p className="absolute right-[-8px] top-[-12px] bg-red-500 text-center text-white rounded-full w-6">
+            0
+          </p>
+        </div>
+      )}
     </div>
   );
 }
